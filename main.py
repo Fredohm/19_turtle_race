e312724b49143b7cbbfdfc6b1a1d1e1efaf7e755
Fredohm <fredwaucquez@hotmail.fr>
2022-01-23 10:59:06 +0100

    RELEASE_1.0

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import turtle
 from turtle import Turtle, Screen
 from random import choice
 
-turtle_list = []
-turtle_speed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-turtle_colors = ["RoyalBlue", "Green", "Crimson", "Yellow", "Fuchsia", "OrangeRed", "DarkTurquoise"]
-
 
 def set_turtle_start(p_turtle, turtle_color, y_pos):
     p_turtle.hideturtle()
@@ -16,29 +12,37 @@ def set_turtle_start(p_turtle, turtle_color, y_pos):
     p_turtle.showturtle()
 
 
+def race():
+    winner_color = ""
+    start_y_pos = -120
+    for turtle_index in range(0, 7):
+        racing_turtle = Turtle(shape="turtle")
+        set_turtle_start(racing_turtle, turtle_colors[turtle_index], start_y_pos)
+        start_y_pos += 40
+        turtle_list.append(racing_turtle)
+
+    is_race_on = True
+    while is_race_on:
+        for turtle_index in range(0, 7):
+            turtle_list[turtle_index].forward(choice(turtle_move))
+
+            if turtle_list[turtle_index].xcor() > 235:
+                is_race_on = False
+                winner_color = turtle_colors[turtle_index]
+
+    if user_bet.lower() == winner_color.lower():
+        print("You win!")
+    else:
+        print("Sorry, you loose!")
+
+
 screen = Screen()
 screen.setup(width=500, height=400)
 
+turtle_list = []
+turtle_move = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+turtle_colors = ["Blue", "Green", "Red", "Yellow", "Violet", "Orange", "Turquoise"]
+
 user_bet = turtle.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-
-start_y_pos = -120
-for turtle_index in range(0, 7):
-    turtle = Turtle(shape="turtle")
-    set_turtle_start(turtle, turtle_colors[turtle_index], start_y_pos)
-    start_y_pos += 40
-    turtle_list.append(turtle)
-
-is_race_on = True
-while is_race_on:
-    for turtle_index in range(0, 7):
-        # TODO fix random speed for each turtle
-        turtle_list[turtle_index].speed(choice(turtle_speed))
-        print(turtle_list[turtle_index].speed(choice(turtle_speed)))
-        turtle_list[turtle_index].forward(5)
-        x_pos = turtle_list[turtle_index].xcor()
-
-        if x_pos > 235:
-            is_race_on = False
-
-
+race()
 screen.exitonclick()
